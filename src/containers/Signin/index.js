17ff66dca33dc1163b88b3586c390b0436c7157f
Neilketchum <daipayanh@gmail.com>
2020-10-04 11:44:07 +0530

    Admin UI Completed

--- src/containers/Signin/index.js
+++ src/containers/Signin/index.js
@@ -1,9 +1,45 @@
 import React from 'react'
-
+import Layout from '../../components/Layouts'
+import { Form, Button, Container, Row, Col } from "react-bootstrap"
+const style = {
+    marginTop: "50px"
+}
 export default function Signin() {
     return (
-        <div>
-            Signin
+        <div >
+
+            <Layout>
+                <Container >
+                    <Row style = {style}>
+                        <Col md={{ span: 6, offset: 3 }}>
+                            <Form>
+                                <Form.Group controlId="formBasicEmail">
+                                    <Form.Label>Email address</Form.Label>
+                                    <Form.Control type="email" placeholder="Enter email" />
+                                    <Form.Text className="text-muted">
+                                        Email is confidential
+                                    </Form.Text>
+                                </Form.Group>
+
+                                <Form.Group controlId="formBasicPassword">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control type="password" placeholder="Password" />
+                                </Form.Group>
+                                <Form.Group controlId="formBasicCheckbox">
+                                    <Form.Check type="checkbox" label="Check me out" />
+                                </Form.Group>
+                                <Button variant="primary" type="submit">
+                                    Submit
+                                </Button>
+                            </Form>
+                        </Col>
+                    </Row>
+
+
+                </Container>
+
+            </Layout>
+
         </div>
     )
 }
--- src/containers/Signup/index.js
+++ src/containers/Signup/index.js
@@ -1,9 +1,54 @@
 import React from 'react'
-
+import Layout from '../../components/Layouts'
+import { Form, Button, Container, Row, Col } from "react-bootstrap"
+const style = {
+    marginTop: "50px"
+}
 export default function Singnup() {
     return (
         <div>
-            Sign UP
+            <Layout>
+                <Container >
+                    <Row style = {style}>
+                        <Col md={{ span: 6, offset: 3 }}>
+                            <Form>
+                                <Row>
+                                    <Col md  = {6}>
+                                        <Form.Label>Email address</Form.Label>
+                                        <Form.Control placeholder="First name" />
+                                    </Col>
+                                    <Col md = {6}>
+                                    <Form.Label>Email address</Form.Label>
+                                        <Form.Control placeholder="Last name" />
+                                    </Col>
+                                </Row>
+                                <br></br>
+                                <Form.Group controlId="formBasicEmail">
+                                    <Form.Label>Email address</Form.Label>
+                                    <Form.Control type="email" placeholder="Enter email" />
+                                    <Form.Text className="text-muted">
+                                        Email is confidential
+                                    </Form.Text>
+                                </Form.Group>
+
+                                <Form.Group controlId="formBasicPassword">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control type="password" placeholder="Password" />
+                                </Form.Group>
+                                <Form.Group controlId="formBasicCheckbox">
+                                    <Form.Check type="checkbox" label="Check me out" />
+                                </Form.Group>
+                                <Button variant="primary" type="submit">
+                                    Submit
+                                </Button>
+                            </Form>
+                        </Col>
+                    </Row>
+
+
+                </Container>
+
+            </Layout>
         </div>
     )
 }
